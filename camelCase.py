a0cfc591746bb6a506d522ce49d37b03391d4c53
Dilli <me@email.com>
2018-02-06 15:04:41 -0600

    Created a test file and updated code in the main program

--- camelCase.py
+++ camelCase.py
@@ -1,13 +1,6 @@
-#define main function
-def main():
-    #call the display function
-    display_banner()
-    #call the function and assigns it to a sentence variable.
-    sentence = instructions()
-    #split the sentence into a list. it removes all the spaces, keeps
-    #only the words and put them in a list.
+
+def camel_case(sentence):
     sentenceList = sentence.split()
-    #set an empty collector to collect string
     collector = ""
     for x in range(len(sentenceList)):
         if x == 0:
@@ -17,19 +10,21 @@ def main():
             firstLetter = word[0]
             remLetters = word[1:]
             collector+=firstLetter.upper()+remLetters.lower()
-    print(collector)
+    return collector
 
-#define display function
-def display_banner():
-    '''Display program name in a banner'''
-    msg = 'AWESOME CamelCaseGenerator PROGRAM'
-    stars = '*' * len(msg)
-    print('\n', stars, '\n', msg, '\n', stars, '\n')
-#this function instructs the user to enter the sentence of user's choice
-#and return that sentence.
-def instructions():
-    print("Enter a sentence to convert to camelcase.")
-    inputSentence = input("Enter your sentence: ")
-    return inputSentence
+#define main function
+def main():
+    #call the display function
+    '''display_banner()'''
+    #call the function and assigns it to a sentence variable.
+    '''sentence = instructions()'''
+    sentence = input('Enter your sentence: ') #this line added
+    #split the sentence into a list. it removes all the spaces, keeps
+    #only the words and put them in a list.
+    '''sentenceList = sentence.split()'''
+    #set an empty collector to collect string
+    camelCased = camel_case(sentence)
+    print(camelCased)
 
-main()
+if __name__ == '__main__':
+    main()
